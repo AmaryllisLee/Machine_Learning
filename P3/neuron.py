9531df79b1f9bf80fb86ad13b33d7b5197643be3
AmaryllisLee <ama.lee@student.hu.nl>
2021-03-08 23:10:51 +0100

    start of backpropagation voor neuron and change parameters to p3 notebook

--- P3/neuron.py
+++ P3/neuron.py
@@ -1,15 +1,17 @@
 class Neuron:
 
-    def __init__(self, list_weights, bias):
+    def __init__(self, list_weights, bias, constant_learning_rate = 0.1):
         self.weights = list_weights
         self.bias    = bias
+        self.n = constant_learning_rate
 
         self.iter = 0
         self.sum_error = 0
         self.rmse = 0
+        self.error = 0
 
     def activation(self,  product_weights):
-        return 1/(1 + 2.71828**(-product_weights)) # sigmoid formule , e = 2.71828
+        return 1/(1 + 2.71828**(-product_weights - self.bias)) # sigmoid formule , e = 2.71828
 
 
     def calculate_output(self, input_arr):
@@ -22,7 +24,42 @@ class Neuron:
             w_som += (weight*x) # increment w_som with the multiplication of weighti and xi
         output = self.activation(w_som) # apply the step function to w_Som
         return output
+
+    def error_output(self, input_arr, d):
+        """Bereken de error van de output neuron
+        De formule voor de error luidt als volgt:
+        o' = a0 * (1-a0) * - (d -  a0)
+        a0 : output van de neuron met de feedforward
+        d : verwachte output
+        """
+        a_out = self.calculate_output(input_arr)
+        self.error = a_out * (1 - a_out) * - (d - a_out)
     
+    def error_hidden(self, input_arr):
+        "Bereken de error van een hidden layer"
+        a_hidden = self.calculate_output(input_arr)
+        self.error = a_hidden * ( 1- a_hidden) * self.error_output()
+
+    def gradient(self, input_arr):
+        "Bereken de gradient van een gewicht tussen neuron i en output neuron j."
+        output = self.calculate_output(input_arr)
+        return  output * self.error
+
+    def delta(self, input_arr): 
+        output = self.calculate_output(input_arr)
+        self.delta_weights = self.n *  output * self.gradient 
+        self.delta_bias    = self.n * self.error
+
+    # Bereken de gewenste aanpassingen aan alle gewichten van output neuron j en sla deze op. 
+    # We voeren de veranderingen nog niet door, omdat we de aanpassingen in het netwerk in één keer (dus allemaal tegelijkertijd) willen doorvoeren.?
+
+    def update(self, input_arr):
+        "Bijwerken vban de weights en de bias "
+        delta_w , delta_b = self.delta(input_arr)
+        for wi in self.weights:
+            wi = wi - delta_w
+        
+        self.bias = self.bias - delta_b
     
     def __str__(self):
         return ("Weights: {}" + "\n" + "Biase/Threshold {}").format(self.weights, self.bias)
@@ -60,8 +97,27 @@ class NeuronNetwork:
             input_arr = input_next_layer# set input_next_layer as input_arr to be used as input for the next layer
         return input_arr
     
-
+    def set_error(self):
+        for layer in self.n_layers:
+            for neuron in layer.n_neurons:
+                neuron.error = 0
     
+    def MSE(self, input_arr):
+        pass
+
+        
+    def train(self,training_examples):
+        cost = None
+        while cost < 0.1:
+            for example in training_examples:
+                self.feed_forward(training_examples)
+                self.set_error()
+                self.update()
+            
+            cost = self.MSE(training_examples) 
+        
+
+        
 
     
     
